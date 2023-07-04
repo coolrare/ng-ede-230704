74f75e79351496ec5a46e9e456de47e5e552cc6d
Will <doggy.huang@gmail.com>
2023-07-04 09:55:42 +0800

    任務02：元件規劃

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { TagsComponent } from './tags/tags.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ArticlesComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule
