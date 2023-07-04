7c5b336929c9f59a4cfaafba22cf70f071a68b36
Will <doggy.huang@gmail.com>
2023-07-04 09:58:04 +0800

    任務03：使用內嵌繫結顯示資料

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-ede-230704';
+  sitename = 'conduit';
+  subtitle = 'A place to share your knowledge.';
 }
